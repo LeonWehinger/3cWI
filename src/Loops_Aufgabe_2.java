296830fe32bd9c0236eb3ef91151068dbd05cf55
Leon <leon.wehinger@htldornbirn.at>
2024-10-03 10:05:42 +0200

    finished_Loops_Aufgabe_2

---
+++ src/Loops_Aufgabe_2.java
@@ -0,0 +1,12 @@
+public class Loops_Aufgabe_2 {
+    public static void main(String[] args) {
+
+       int i = 2;
+       int P = 0;
+        for (i = 2; i <= 1000; i = i+2){
+            P += i;
+        }
+
+        System.out.println(P);
+    }
+}
