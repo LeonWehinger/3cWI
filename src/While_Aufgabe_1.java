22a6216e71acf94765db99cadabe55c8bdc7dc1b
Leon <leon.wehinger@htldornbirn.at>
2024-10-09 09:02:40 +0200

    While_Aufgabe_1 finished

--- src/While_Aufgabe_1.java
+++ src/While_Aufgabe_1.java
@@ -1,2 +1,29 @@
+import java.util.Random;
+
 public class While_Aufgabe_1 {
+    public static void main(String[] args) {
+
+
+
+        boolean isfinished = true;
+        int summe = 0;
+
+        while (isfinished){
+
+            Random random = new Random();
+            int rndm = random.nextInt(21)+10;
+            summe += rndm;
+            System.out.println(rndm);
+
+            if (rndm == 15 || rndm == 25){
+                isfinished = false;
+                System.out.println(summe);
+            }
+        }
+
+
+
+
+
+    }
 }
