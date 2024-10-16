1a7c285cc1a7c09b6a34eedd1483494110937794
Leon <leon.wehinger@htldornbirn.at>
2024-10-16 09:30:42 +0200

    Wuerfelspiel almost finished

--- src/Wuerfelspiel.java
+++ src/Wuerfelspiel.java
@@ -7,19 +7,46 @@ public class Wuerfelspiel {
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        boolean weiter = true;
-        int auswahl = scanner.nextInt();
-        int würfeln = random.nextInt(6) + 1;
-        int würfelsumme = 0;
+        boolean nejo = true;
+        while (nejo) {
+            int playerwuerfelsumme = 0;
+            int pcwuerfelsumme = 0;
 
-        while (weiter) {
-            System.out.println("1 zum Würfeln");
-            System.out.println("2 zum Beenden");
+            for (int i = 0; i < 6; i++) {
+                int playerwuerfeln = random.nextInt(6) + 1;
+                int pcwuerfeln = random.nextInt(6) + 1;
 
-            if (auswahl == 1) {
-                System.out.println(würfeln);
-                würfelsumme += würfeln;
+
+                System.out.println("1 zum Wuerfeln");
+                System.out.println("2 zum Beenden");
+                int auswahl = scanner.nextInt();
+
+
+                if (auswahl == 1) {
+                    System.out.println("Du hast " + playerwuerfeln + " gewuerfelt");
+                    playerwuerfelsumme += playerwuerfeln;
+                    System.out.println("Deine Wuerfelsumme: " + playerwuerfelsumme);
+
+                    System.out.println("Der Computer hat " + pcwuerfeln + " gewuerfelt");
+                    pcwuerfelsumme += pcwuerfeln;
+                    System.out.println("Seine Wuerfelsumme: " + pcwuerfelsumme);
+                }
             }
+            if (playerwuerfelsumme > pcwuerfelsumme) {
+                System.out.println("Du hast gewonnen!");
+            } else if (pcwuerfelsumme > playerwuerfelsumme) {
+                System.out.println("Du hast verloren!");
+            } else if (pcwuerfelsumme == playerwuerfelsumme) {
+                System.out.println("Gleichstand!");
+            } else {
+                System.out.println("Fehler, Nejo schuld!");
+            }
+            System.out.println("");
+            int auswahl2 = scanner.nextInt();
+
+            nejo = false;
+
         }
     }
 }
+
