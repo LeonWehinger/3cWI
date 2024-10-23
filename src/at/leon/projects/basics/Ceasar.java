61c828de8f0f609adf6c25bae4bbce157a4c4710
Leon <leon.wehinger@htldornbirn.at>
2024-10-23 09:30:07 +0200

    lesson end

---
+++ src/at/leon/projects/basics/Ceasar.java
@@ -0,0 +1,17 @@
+package at.leon.projects.basics;
+
+public class Ceasar {
+    public static void main(String[] args) {
+        String result = encrypt("abc");
+        System.out.println(result);
+
+    }
+
+    public static String encrypt (String data) {
+        return "";
+    }
+
+    public static String decrypt (String data) {
+        return "";
+    }
+}
