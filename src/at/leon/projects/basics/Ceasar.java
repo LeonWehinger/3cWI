3dfd40ff1241b621702b098c7eb50617f001099c
Leon <leon.wehinger@htldornbirn.at>
2024-11-06 09:29:54 +0100

    lesson end

--- src/at/leon/projects/basics/Ceasar.java
+++ src/at/leon/projects/basics/Ceasar.java
@@ -1,13 +1,29 @@
 package at.leon.projects.basics;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
+import static java.awt.SystemColor.text;
+
 public class Ceasar {
     public static void main(String[] args) {
-        String result = encrypt("abc");
-        System.out.println(result);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Gib einen Text ein!");
+        String Text = scanner.nextLine();
+        String encrypted = encrypt(Text);
+        System.out.println(encrypted);
 
     }
 
     public static String encrypt (String data) {
+    String encrypted = "";
+        for (int i = 0; i < data.length(); i++){
+            char ch = data.charAt(i);
+            encrypted = encrypted + (ch);
+        }
+
+
+
         return "";
     }
 
