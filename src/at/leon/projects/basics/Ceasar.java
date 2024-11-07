46cc13dc51c13ca105217d8877866f8e155a26a8
Leon <leon.wehinger@htldornbirn.at>
2024-11-07 08:37:55 +0100

    Ceasar finished

--- src/at/leon/projects/basics/Ceasar.java
+++ src/at/leon/projects/basics/Ceasar.java
@@ -8,26 +8,34 @@ import static java.awt.SystemColor.text;
 public class Ceasar {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Gib einen Text ein!");
+        System.out.println("String eingeben!");
         String Text = scanner.nextLine();
-        String encrypted = encrypt(Text);
-        System.out.println(encrypted);
+        System.out.println("Shift eingeben!");
+        int Shift = scanner.nextInt();
+        String encr = encrypt(Text, Shift);
+        System.out.println(encr);
+        String decr = decrypt(encr, Shift);
+        System.out.println(decr);
 
     }
 
-    public static String encrypt (String data) {
-    String encrypted = "";
-        for (int i = 0; i < data.length(); i++){
+    public static String encrypt(String data, int Shift) {
+        String encr = "";
+        for (int i = 0; i < data.length(); i++) {
             char ch = data.charAt(i);
-            encrypted = encrypted + (ch);
+            encr = encr + (char) (ch + Shift);
         }
 
 
-
-        return "";
+        return encr;
     }
 
-    public static String decrypt (String data) {
-        return "";
+    public static String decrypt(String data, int Shift) {
+        String decr = "";
+        for (int i = 0; i < data.length(); i++) {
+            char ch = data.charAt(i);
+            decr = decr + (char) (ch - Shift);
+        }
+        return decr;
     }
 }
