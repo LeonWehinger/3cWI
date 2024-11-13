dac646bf7432e407617eb9503a44d7b52a4f7410
Leon <leon.wehinger@htldornbirn.at>
2024-11-13 09:33:01 +0100

    lesson finished

--- src/at/leon/projects/basics/Woerter_Raten.java
+++ src/at/leon/projects/basics/Woerter_Raten.java
@@ -9,17 +9,33 @@ public class Woerter_Raten {
         char[] hiddenarray = new char[wordarray.length];
 
         System.out.println(hiddenarray);
-        for (int i = 0; i < hiddenarray.length; i++){
+        for (int i = 0; i < hiddenarray.length; i++) {
             hiddenarray[i] = '*';
         }
 
         Scanner scanner = new Scanner(System.in);
 
         boolean finished = false;
-        while (!finished){
+        while (!finished) {
+            System.out.println(hiddenarray);
+            System.out.println("Gib einen Buchstaben ein!");
+            String input = scanner.nextLine();
+            char guess = input.charAt(0);
+            for (int i = 0; i < wordarray.length; i++) {
+                if (guess == wordarray[i]) {
+                    hiddenarray[i] = guess;
+                }
+                if (hiddenarray == wordarray) {
+                    System.out.println("Das Wort war:" + Word);
+                    finished = true;
+                }
 
+            }
         }
 
 
     }
+
+
 }
+
