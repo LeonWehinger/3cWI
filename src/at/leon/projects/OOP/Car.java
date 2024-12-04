7de601e7e78585df576e45c1813f628c798cdf7c
Leon <leon.wehinger@htldornbirn.at>
2024-12-04 09:22:55 +0100

    OO_03 finished

--- src/at/leon/projects/OOP/Car.java
+++ src/at/leon/projects/OOP/Car.java
@@ -3,6 +3,45 @@ package at.leon.projects.OOP;
 public class Car {
     public String brand;
     public int fuelConsumption;
+    public int fuelAmount;
+    public int maxFuelAmount;
     public String serialNumber;
     public String color;
+
+
+    public void drive() {
+        this.fuelAmount = this.fuelAmount - this.fuelConsumption;
+        System.out.println("I am driving!");
+    }
+
+    public void dobreak() {
+        System.out.println("I am braking!");
+    }
+
+    public void turboBoost() {
+        if (this.fuelAmount >= this.maxFuelAmount * 0.1) {
+            System.out.println("SuperBoostMode");
+        } else {
+            System.out.println("Not enough fuel to go to Superboost");
+        }
+    }
+
+    public void honk(int amountOfRepetitions) {
+        for (int i = 0; i < amountOfRepetitions; i++) {
+            System.out.print("Tuuut ");
+        }
+    }
+
+    public void getRemainingRange() {
+        double RemainingRange = this.fuelAmount / this.fuelConsumption;
+        System.out.println("The remaining range is: " + RemainingRange);
+    }
+
+
+    //public Car(String brand, double fuelConsumption, double fuelAmount, String serialNumber, String color) {
+    // this.brand = brand;
+    // this.fuelConsumption = fuelConsumption;
+    //  this.serialNumber = serialNumber;
+    // this.color = color;
+    //}
 }
--- src/at/leon/projects/OOP/main.java
+++ src/at/leon/projects/OOP/main.java
@@ -3,20 +3,32 @@ package at.leon.projects.OOP;
 public class main {
 
     public static void main(String[] args) {
+
         Car car1 = new Car();
         car1.brand = "Porsche";
         car1.fuelConsumption = 8;
+        car1.fuelAmount = 160;
+        car1.maxFuelAmount = 160;
         car1.serialNumber = "P1234";
         car1.color = "black";
 
         Car car2 = new Car();
         car2.brand = "VW";
         car2.fuelConsumption = 5;
+        car2.fuelAmount = 120;
+        car2.maxFuelAmount = 120;
         car2.serialNumber = "V1235";
         car2.color = "yellow";
 
 
-        System.out.println(car2.serialNumber +"\n"+car1.brand);
+        System.out.println(car2.fuelAmount);
+        car2.getRemainingRange();
+        car2.drive();
+        car2.getRemainingRange();
+        System.out.println(car2.fuelAmount);
+        car2.turboBoost();
+        car2.dobreak();
+        car2.honk(1);
 
 
     }
