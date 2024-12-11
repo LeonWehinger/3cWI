923cbfca937e19f3fcae69732a3d0dfff841b39b
Leon <leon.wehinger@htldornbirn.at>
2024-12-11 08:17:32 +0100

    OO_04.2 finished

--- src/at/leon/projects/OOP/Car.java
+++ src/at/leon/projects/OOP/Car.java
@@ -48,6 +48,9 @@ public class Car {
     }
 
 
+
+
+
     public String getBrand() {
         return brand;
     }
--- src/at/leon/projects/OOP/main.java
+++ src/at/leon/projects/OOP/main.java
@@ -6,10 +6,12 @@ public class main {
 
         Car car1 = new Car("Porsche",7,140,140,"P1234","black");
         System.out.println(car1.getBrand());
+        car1.setColor("blue");
         System.out.println(car1.getColor());
         System.out.println(car1.getFuelAmount());
         System.out.println(car1.getSerialNumber());
         System.out.println(car1.getColor());
+        car1.getRemainingRange();
 
 
 
